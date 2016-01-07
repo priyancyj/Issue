95edd4c3e4a05f3bf7f6b7667fc0240ae36878b1
Priyancy Jain <priyancyjain.iitkgp@gmail.com>
2016-01-07 11:38:08 +0000

    edit commit

--- Issue2/Task2.R
+++ Issue2/Task2.R
@@ -1,43 +1,28 @@
 #' Gives average of delivery time per week
 #'
+#' @param df: dataframe which contains all sales information
+#' @return plot of average delivery time over weeks
 #'
-#'
-#'
-#'
-#'
+#' @examples
+#' Delivery_time_weekly(sales())
 Delivery_time_weekly <- function(df) {
   x <- filter(df, delivery_time < 600)
   deliver <- select(x, week, delivery_time) %>%
     group_by(week) %>%
     summarise(mean(delivery_time))
-  return(deliver)
+  plot(deliver, type = "o", col = "blue")
 }
-#' Reads table delivery
-#'
-#'
-#'
-#'
-#' @examples
-#' delivery(sales())
-delivery <- function(df) {
-  drv <- DBI::dbDriver("PostgreSQL")
-  con <- RPostgreSQL::dbConnect(drv, dbname="mom", user = "metar", host="localhost", password = "123$")
-  on.exit(RPostgreSQL::dbDisconnect(con))
-  locations <- RPostgreSQL::dbReadTable(con, c("mom", "locations"))
-  vehicle_location <- RPostgreSQL::dbReadTable(con, c("mom", "vehicle_locations"))
-  start_latitude <- filter(vehicle_location, id == 1)$latitude
-  start_longitude <- filter(vehicle_location, id == 1)$longitude
 
-  select(sales(), id, delivery_location_id, delivery_time
-
-
-#' Gives the dustance in kilometres of two lat/long points
+#' Euclidean distance
 #'
-#' @param
-#' @return
+#' @param long1: longitude of location 1
+#' @param long2: longitude of location 2
+#' @param lat1: latitude of location 1
+#' @param lat2: latitude of location 2
+#' @return Euclidean distance in kilometres between two points
 #'
 #' @example
-#'
+#' dist_km(77, 12, 78, 13)
 dist_km <- function (long1, lat1, long2, lat2) {
   rad <- pi/180
   a1 <- lat1 * rad
@@ -53,19 +38,41 @@ dist_km <- function (long1, lat1, long2, lat2) {
   return(d)
 }
 
-
-
-
-
-
-
-
-#' Gives the analysis of delivery time over Distance
+#' delivery
 #'
 #' @param
-#' @return
+#' @return Returns a dataframe which contains all information regarding Delivery
 #'
 #' @examples
+#' delivery()
+delivery <- function() {
+  drv <- DBI::dbDriver("PostgreSQL")
+  con <- RPostgreSQL::dbConnect(drv, dbname="mom", user = "metar", host="localhost", password = "123$")
+  on.exit(RPostgreSQL::dbDisconnect(con))
+  locations <- RPostgreSQL::dbReadTable(con, c("mom", "locations"))
+  vehicle_location <- RPostgreSQL::dbReadTable(con, c("mom", "vehicle_locations"))
+  lat1 <- filter(vehicle_location, id == 1)$latitude
+  long1 <- filter(vehicle_location, id == 1)$longitude
+  delivery <- select(sales(), id, delivery_location_id, delivery_time) %>%
+    filter(delivery_time < 600) %>%
+    left_join(locations, by = c("delivery_location_id" = "id")) %>%
+    rename(order_id = id, user_id = person_id) %>%
+    mutate(distance = dist_km(long1, lat1, longitude, latitude))
+  return(delivery)
+}
+
+#' Gives the analysis of delivery time over Eucledian Distance
 #'
-Delivery_over_distance
+#' @param df: dataframe which contains all information regarding delivery
+#' @return plot of average delivery time over Eucledian distance
 #'
+#' @examples
+#' delivery_over_distance(delivery())
+delivery_over_distance <- function(df) {
+  a <- select(df, distance, delivery_time) %>%
+    group_by(distance) %>%
+    summarise(Delivery_time = mean(delivery_time))
+  plot(a, type = "o", col = "blue")
+}
+
+
